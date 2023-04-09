70f8661dd924e3ebf839e04fad3ed3d717d48ff5
Gwion Tangi <lil.smiss7@gmail.com>
2023-04-09 18:57:50 +0200

    added pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import { IonicVue } from '@ionic/vue';
 
+import { createPinia } from "pinia";
+
 import App from './App.vue';
 import router from './router';
 
@@ -23,7 +25,10 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const app = createApp(App).use(IonicVue).use(router);
+const app = createApp(App)
+  .use(IonicVue)
+  .use(router)
+  .use(createPinia());
 
 router.isReady().then(() => {
   app.mount('#app');
