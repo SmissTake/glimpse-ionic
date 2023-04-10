152c97ba38080911c2d7b36921c74e834b738883
Gwion Tangi <lil.smiss7@gmail.com>
2023-04-10 18:35:59 +0200

    comment value in log, waiting for route

--- src/components/CommentInput.vue
+++ src/components/CommentInput.vue
@@ -3,7 +3,7 @@
         <ion-item>
           <ion-input label="Commentaire" label-placement="stacked" placeholder="Ajouter un commentaire"></ion-input>
         </ion-item>
-        <ion-button>
+        <ion-button @click="postComment">
             <ion-icon slot="icon-only" :icon="sendOutline"></ion-icon>
         </ion-button>
     </ion-row>
@@ -21,6 +21,21 @@ export default defineComponent({
         IonInput,
         IonRow,
         IonButton,
+    },
+    props: {
+        idPlace: {
+            type: Number,
+            required: true,
+        },
+    },
+    methods: {
+        postComment() {
+            //get data from input
+            if(!document.querySelector('ion-input')) return;
+            if(!document.querySelector('ion-input')?.value) return;
+            const comment = document.querySelector('ion-input')?.value;
+            console.log('postComment: ' + comment + ' for place ' + this.idPlace);
+        },
     },
     data() {
         return {
