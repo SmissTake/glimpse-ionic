dfca10b73573bc7bf47545056c5d5878b703eb78
Gwion Tangi <lil.smiss7@gmail.com>
2023-04-15 15:31:58 +0200

    added post comment

--- src/components/CommentInput.vue
+++ src/components/CommentInput.vue
@@ -1,11 +1,20 @@
 <template>
     <ion-row>
-        <ion-item>
-          <ion-input label="Commentaire" label-placement="stacked" placeholder="Ajouter un commentaire"></ion-input>
-        </ion-item>
-        <ion-button @click="postComment">
-            <ion-icon slot="icon-only" :icon="sendOutline"></ion-icon>
-        </ion-button>
+        <form @submit="submitComment">
+            <ion-item>
+                <ion-input 
+                label="Commentaire"
+                label-placement="stacked"
+                placeholder="Ajouter un commentaire"
+                name="comment"
+                :required="true"
+                :value="commentForm.comment"
+                @ionInput="onInput($event)"></ion-input>
+            </ion-item>
+            <ion-button type="submit">
+                <ion-icon slot="icon-only" :icon="sendOutline"></ion-icon>
+            </ion-button>
+        </form>
     </ion-row>
 </template>
 
@@ -30,17 +39,41 @@ export default defineComponent({
         },
     },
     methods: {
-        postComment() {
-            //get data from input
-            if(!document.querySelector('ion-input')) return;
-            if(!document.querySelector('ion-input')?.value) return;
-            const comment = document.querySelector('ion-input')?.value;
-            console.log('postComment: ' + comment + ' for place ' + this.idPlace);
+        onInput(event: any) {
+            const name = event.target.name;
+            const value = event.target.value.trim();
+            if(value === '') return;
+            this.commentForm = { ...this.commentForm, [name]: value };
+            console.log(this.commentForm);
+        },
+        submitComment() {
+            const data = new FormData();
+            data.append('comment', this.commentForm.comment);
+            data.append('usersId', this.commentForm.usersId);
+            data.append('placesId', JSON.stringify(this.commentForm.placesId));
+
+            fetch(`${process.env.VUE_APP_API_URL}/comment/create`, {
+                method: 'POST',
+                body: data,
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
         },
     },
     data() {
         return {
             sendOutline,
+            commentForm: {
+                comment: '',
+                images: [],
+                usersId: `${process.env.VUE_APP_USER_ID}`,
+                placesId: this.idPlace,
+            }
         };
     },
 });
