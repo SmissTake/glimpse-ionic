f068cd9dbbd21293d648e9d6841f330a30c6133c
Gwion Tangi <lil.smiss7@gmail.com>
2023-04-24 19:18:03 +0200

    added service worker to app

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { createPinia } from "pinia";
 import App from './App.vue';
 import router from './router';
 
+import './registerServiceWorker';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
