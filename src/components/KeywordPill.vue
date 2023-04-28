d20497a630e687bf2e69d66dcf4e3edbd88279ea
Gwion Tangi <lil.smiss7@gmail.com>
2023-04-28 16:10:05 +0200

    new component KeywordPill

---
+++ src/components/KeywordPill.vue
@@ -0,0 +1,37 @@
+<template>
+    <ion-chip>
+        <p>
+            {{ keyword }}
+        </p>
+    </ion-chip>
+</template>
+
+<script lang="ts">
+import { IonChip } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'KeywordPill',
+    components: { IonChip },
+    props: {
+        keyword: {
+            type: String,
+            required: true,
+        },
+    },
+});
+
+</script>
+
+<style scoped>
+    ion-chip {
+        --background: var(--ion-color-secondary);
+        --color: var(--ion-color-secondary-contrast);
+        border-radius: 0.5em;
+    }
+
+    /* ion chip text in  --font: var(--ion-text-paragraph); */
+    ion-chip p {
+        font: var(--ion-text-paragraph);
+    }
+</style>
