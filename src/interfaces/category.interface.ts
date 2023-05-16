dbb7536f09c3c184a33005ec5e4e8e01de8dd7cd
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-16 12:24:05 +0200

    added new data to category

--- src/interfaces/category.interface.ts
+++ src/interfaces/category.interface.ts
@@ -1,4 +1,13 @@
 export interface Category {
     id: number;
     label: string;
+    avatar: string;
+    numberPlaces: number;
+    Place: {
+        id: number;
+        numberLike: number;
+        PicturePlaces: {
+            url: string;
+        }
+    }
 }
