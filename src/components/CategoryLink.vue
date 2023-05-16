a38e48bf46c66a86ef6e6614425fa4febe165bf0
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-16 21:14:25 +0200

    added style and options

--- src/components/CategoryLink.vue
+++ src/components/CategoryLink.vue
@@ -1,12 +1,13 @@
 <template>
-  <ion-avatar v-if="category.avatar">
+  <ion-avatar 
+    v-if="category.avatar"
+    :class="style + ' ' + size">
     <div class="circle">
       <img
       :src="imageSource+'/'+category.avatar"
-        class="colored-svg"
       />
     </div>
-    <label>
+    <label v-if="label">
       {{ category.label }}
     </label>
   </ion-avatar>
@@ -22,6 +23,18 @@
       category: {
         type: Object as () => Category,
         required: true,
+      },
+      style: {
+        type: String,
+        default: 'green',
+      },
+      label: {
+        type: Boolean,
+        default: true,
+      },
+      size: {
+        type: String,
+        default: 'medium',
       },
     },
     components: {
@@ -50,21 +63,45 @@
     font: var(--ion-text-label);
   }
   .circle {
-    width: 50px;
-    height: 50px;
     border-radius: 50%;
-    background-color: #fff;
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+  .medium .circle {
+    width: 50px;
+    height: 50px;
+  }
+  .small .circle {
+    width: 30px;
+    height: 30px;
+  }
+  .green .circle {
+    background-color: #fff;
     filter: drop-shadow(0px 0px 7px rgba(0, 0, 0, 0.1));
   }
   ion-avatar img {
-    width: 30px;
-    height: 30px;
     object-fit: contain;
   }
-  .colored-svg {
+
+  .medium img {
+    width: 30px;
+    height: 30px;
+  }
+
+  .small img {
+    width: 20px;
+    height: 20px;
+  }
+  .green img{
     filter: invert(60%) sepia(36%) saturate(485%) hue-rotate(36deg) brightness(96%) contrast(91%);
+  }
+
+  .simple .circle {
+    border: 1px solid #fff;
+    filter: drop-shadow(0px 0px 7px rgba(0, 0, 0, 0.1));
+  }
+  .white-svg {
+    filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%);
   }
 </style>
